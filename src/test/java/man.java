1c9165f9c1b6782b70b833a8e2a43807553c3301
manik <manikantatoleti28@gmail.com>
2022-11-03 12:23:21 +0530

    architec asian development

--- src/test/java/man.java
+++ src/test/java/man.java
@@ -1,9 +1,18 @@
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
 
 public class man {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(",anikanta");
+		System.out.println("manikanta");
+		System.out.println("gopal");
+		System.out.println("niharika");
+
+		System.out.println("shanmukh");
+		System.out.println("nikil");
+		
+		
 
 	}
 
